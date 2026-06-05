f97162c4bf03500481931089a254cfbbdf9dd20c
张云腾 <15234711471@163.com>
2026-06-05 08:16:22 +0800

    add(pid):add PID integral limiter

---
+++ 0-6位置式PID定速控制-积分限幅/User/main.c
@@ -0,0 +1,123 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "LED.h"
+#include "Timer.h"
+#include "Key.h"
+#include "RP.h"
+#include "Motor.h"
+#include "Encoder.h"
+#include "Serial.h"
+#include <math.h>
+
+#define EPSION 0.0001
+uint8_t KeyNum;
+uint8_t MotorFlag = 1;
+float Target,Actual,Out;
+float Kp ,Ki ,Kd ;
+float Error0,Error1,ErrorInt;
+
+int main()
+{
+	LED_Init();
+	OLED_Init();
+	Key_Init();
+	Motor_Init();
+	Encoder_Init();
+	Serial_Init();
+	RP_Init();
+	Timer_Init();
+	OLED_Printf(0,0,OLED_8X16,"Speed Control");
+	OLED_Update();
+	while(1)
+	{
+		KeyNum = Key_GetNum();
+		if(KeyNum == 1)
+		{
+			MotorFlag = !MotorFlag;
+		}
+		if(MotorFlag)
+		{
+			LED_ON();
+		}
+		else
+		{
+			LED_OFF();
+		}
+		Kp = RP_GetValue(1) /4095.0 *2;
+		Ki = RP_GetValue(2) /4095.0 *2;
+		Kd = RP_GetValue(3) /4095.0 *2;
+		Target = RP_GetValue(4)/4095.0 * 300 -150;
+		OLED_Printf(0,16,OLED_8X16,"Kp:%4.2f",Kp);
+		OLED_Printf(0,32,OLED_8X16,"Ki:%4.2f",Ki);
+		OLED_Printf(0,48,OLED_8X16,"Kd:%4.2f",Kd);
+		
+		OLED_Printf(64,16,OLED_8X16,"Tar:%+04.0f",Target);
+		OLED_Printf(64,32,OLED_8X16,"Act:%+04.0f",Actual);
+		OLED_Printf(64,48,OLED_8X16,"Out:%+04.0f",Out);
+		OLED_Update();
+		Serial_Printf("%f,%f,%f,%f\r\n",Target,Actual,Out,ErrorInt);
+	}
+	
+}
+
+void TIM1_UP_IRQHandler(void)
+{
+	static uint16_t Count;
+	if (TIM_GetITStatus(TIM1, TIM_IT_Update) == SET)
+	{
+		
+		Key_Tick();
+		Count++;
+		if(Count>=40)
+		{
+			Count = 0;
+			
+			Actual = Encoder_Get();
+			
+			//获取本次误差和上次误差
+			Error1 = Error0;
+			Error0 = Target - Actual;
+			
+			//误差积分
+			if(fabs(Ki) > EPSION)
+			{
+				
+				ErrorInt += Error0;
+			}
+			else
+			{
+				ErrorInt = 0;
+			}
+			if(ErrorInt >500)
+			{
+				ErrorInt = 500;
+			}
+			if(ErrorInt <-500)
+			{
+				ErrorInt = -500;
+			}
+			Out = Kp * Error0 + Ki *ErrorInt + Kd *(Error0 - Error1);
+			
+			//输出限幅
+			if(Out>100)
+			{
+				Out = 100;
+			}
+			if(Out<-100)
+			{
+				Out = -100;
+			}
+			if(MotorFlag)
+			{
+				Motor_SetPWM(Out);
+			}
+			else
+			{
+				Motor_SetPWM(0);
+			}
+		}
+		TIM_ClearITPendingBit(TIM1, TIM_IT_Update);
+	}
+}
+
